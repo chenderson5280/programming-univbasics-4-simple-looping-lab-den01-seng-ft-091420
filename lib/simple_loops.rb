46437138ff7afc528bd38a853dc6784613180425
Christopher Henderson <chenderson5280@gmail.com>
2020-09-02 22:25:40 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -19,7 +19,7 @@ count = 0
  while count <array.length do
 puts array[count]
 count+=1
-end
+ end
 end
 
 
@@ -27,7 +27,7 @@ def return_string_array(array)
 count = 0
  while  count < array.length     do
 
-puts array[count].to_s
+puts array[count]
    count +=1
-end
+ end
 end
